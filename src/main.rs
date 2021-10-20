1ccbcd11f67d01318b51e0ddf1b4834a473cbe4c
unknown <13christopherb@gmail.com>
2021-10-19 20:45:12 -0700

    Activation function/dot prodct

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,22 @@
 #[inline]
-fn sigmoid(x: f64) -> f64 {
-    return 1. / (1. + ((-1. * x).exp()));
+fn sigmoid(x: f64) -> f64 { 1. / (1. + ((-1. * x).exp())) }
+
+#[inline]
+fn sigmoid_prime(x: f64) -> f64 { sigmoid(x) * (1. - sigmoid(x)) }
+
+fn evaluate_layer(weights: &[f64], inputs: &[f64]) -> f64 {
+    let mut product :f64 = 0.0;
+    for i in 0..inputs.len() {
+        product += weights[i] * inputs[i];
+    }
+    sigmoid(product)
 }
 
 fn main() {
-    let x = sigmoid(0.5);
-    println!("{}",x.to_string());
+    let weights = [0.5, 0.5, 0.5];
+    let inputs = [0.5, 0.5, 0.5];
+
+    let output = evaluate_layer(&weights, &inputs);
+    println!("{}",output.to_string());
 
 }
