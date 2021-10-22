123804e20883a3844347751620a324f12a75278f
unknown <13christopherb@gmail.com>
2021-10-22 11:10:17 -0700

    Add some unit tests for matrix

--- src/matrix.rs
+++ src/matrix.rs
@@ -1,22 +1,21 @@
-
-
+#[derive(Debug, PartialEq, PartialOrd, Eq)]
 pub struct Matrix<T> {
     pub data: Vec<T>,
     cols: usize,
     rows: usize,
 }
 
-impl<T: Copy + Clone + std::ops::Mul<Output = T>> Matrix<T> {
+impl<T: Copy + Clone + std::ops::Mul<Output=T>> Matrix<T> {
     pub fn cols(&self) -> usize { self.cols }
 
     pub fn rows(&self) -> usize { self.rows }
 
-    pub fn len(&self) -> usize  {self.data.len()}
+    pub fn len(&self) -> usize { self.data.len() }
 
     pub fn transpose(&self) -> Matrix<T> {
         let mut transpose: Vec<T> = Vec::new();
-        for i in 0 .. self.cols{
-            for j in 0 .. self.rows {
+        for i in 0..self.cols {
+            for j in 0..self.rows {
                 transpose.push(self.data[j * self.cols + i].clone());
             }
         }
@@ -25,8 +24,8 @@ impl<T: Copy + Clone + std::ops::Mul<Output = T>> Matrix<T> {
 
     pub fn slice(&self, rows: (usize, usize), cols: (usize, usize)) -> Matrix<T> {
         let mut out: Vec<T> = Vec::new();
-        for i in rows.0 .. rows.1 {
-            for j in cols.0 .. cols.1 {
+        for i in rows.0..rows.1 {
+            for j in cols.0..cols.1 {
                 out.push(self.data[i * self.cols + j].clone());
             }
         }
@@ -43,7 +42,7 @@ impl<T: Copy + Clone + std::ops::Mul<Output = T>> Matrix<T> {
 
     fn hadamard_product(a: &[T], b: &[T]) -> Vec<T> {
         let mut out: Vec<T> = Vec::new();
-        for i in 0 .. a.len() {
+        for i in 0..a.len() {
             out.push(a[i] * b[i]);
         }
         out
@@ -51,15 +50,15 @@ impl<T: Copy + Clone + std::ops::Mul<Output = T>> Matrix<T> {
 }
 
 
-    impl<T: Copy + std::ops::Mul<Output = T> + std::ops::Add<Output = T>> std::ops::Mul<Matrix<T>> for Matrix<T> {
+impl<T: Copy + std::ops::Mul<Output=T> + std::ops::Add<Output=T>> std::ops::Mul<Matrix<T>> for Matrix<T> {
     type Output = Matrix<T>;
 
     fn mul(self, rhs: Matrix<T>) -> Matrix<T> {
         let mut out: Vec<T> = Vec::new();
-        for i in 0 .. self.rows {
-            for j in 0 .. rhs.cols {
+        for i in 0..self.rows {
+            for j in 0..rhs.cols {
                 let mut sum = self[[i, 0]] * rhs[[0, j]];
-                for k in 1 .. rhs.rows {
+                for k in 1..rhs.rows {
                     sum = sum + self[[i, k]] * rhs[[k, j]];
                 }
                 out.push(sum);
@@ -73,7 +72,7 @@ impl<T: Copy + Clone + std::ops::Mul<Output = T>> Matrix<T> {
     }
 }
 
-impl<T: Copy + std::ops::Mul<Output = T> + std::ops::Add<Output = T>> std::ops::Mul<T> for Matrix<T> {
+impl<T: Copy + std::ops::Mul<Output=T> + std::ops::Add<Output=T>> std::ops::Mul<T> for Matrix<T> {
     type Output = Matrix<T>;
 
     fn mul(self, rhs: T) -> Matrix<T> {
@@ -94,5 +93,42 @@ impl<T> std::ops::Index<[usize; 2]> for Matrix<T> {
 
     fn index(&self, idx: [usize; 2]) -> &T {
         &self.data[idx[0] * self.cols + idx[1]]
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::Matrix;
+
+    #[test]
+    fn test_create_matrix() {
+        let values = vec![0,1,2,3,4,5];
+        let matrix: Matrix<u64> = Matrix::new(values,  3, 3);
+        assert_eq!(matrix.data,
+                   vec![0,1,2,3,4,5],
+                   "New matrix did not contain correct data"
+        );
+        assert_eq!(matrix.rows(),
+                   3,
+                   "Matrix did not have correct number of rows, it had '{}' \
+                   rows while 3 were expected",
+                   matrix.rows());
+        assert_eq!(matrix.cols(),
+                   3,
+                   "Matrix did not have correct number of columns, it had '{}' \
+                   rows while 3 were expected",
+                   matrix.cols());
+    }
+
+    #[test]
+    fn test_scalar_multiplication() {
+        let values = vec![0,1,2,3,4,5];
+        let matrix: Matrix<u64> = Matrix::new(values,  3, 3);
+        let expected_values = vec![0,3,6,9,12,15];
+        let expected: Matrix<u64> = Matrix::new(expected_values,  3, 3);
+        let product = matrix * 3;
+        assert_eq!(product,
+                   expected,
+        );
     }
 }
